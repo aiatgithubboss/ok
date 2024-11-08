53063d414dd65b3859fbffcf22d06e8caea61d71
Aiat <aziatskitigr98@gmail.com>
2024-11-09 01:47:57 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll("nav ul li a");
+
+    links.forEach(link => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            const target = document.querySelector(link.getAttribute("href"));
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: "smooth"
+            });
+        });
+    });
+});
+
